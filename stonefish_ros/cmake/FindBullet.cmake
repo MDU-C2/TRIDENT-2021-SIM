17d80ac7e8885625b36d3c4c80f0302866088516
Felix Sjoqvist <fst17001@student.mdh.se>
2021-11-01 18:32:26 +0100

    delete stonefish ros

--- stonefish_ros/cmake/FindBullet.cmake
+++
@@ -1,9 +0,0 @@
-
-list (APPEND BULLET_LIBRARIES LinearMath)
-#list (APPEND BULLET_LIBRARIES Bullet3Common)
-#list (APPEND BULLET_LIBRARIES BulletInverseDynamics)
-list (APPEND BULLET_LIBRARIES BulletCollision)
-list (APPEND BULLET_LIBRARIES BulletDynamics)
-#list (APPEND BULLET_LIBRARIES BulletSoftBody)
-
-set(BULLET_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/external/stonefish/3rdparty)
